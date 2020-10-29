60fa4aa47ea3288b9e742c19312e0708adb2ff71
David Zinser <dmzinser@gmail.com>
2020-10-29 15:04:12 -0700

    NPM Updates due to inability to execute 'npm run build' command.

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,6 +2,7 @@ import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
+
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
